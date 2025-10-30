7330cdb2e14d4225762431cfc95730d33fcbe358
Kaamibroken <kaamibroken64@gmail.com>
2025-10-31 01:13:38 +0500

    Update otp.js

--- api/otp.js
+++ api/otp.js
@@ -4,19 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default async function handler(req, res) {
+  // Sirf POST method allow
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   try {
-    const response = await fetch("http://51.89.99.105/NumberPanel/client/SMSCDRStats", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": `PHPSESSID=${process.env.PHPSESSID}`
-      },
-      body: "fdate1=2025-10-30+00%3A00%3A00&fdate2=2025-10-30+23%3A59%3A59&frange=&fnum=&fcli="
-    });
+    const response = await fetch(
+      "http://51.89.99.105/NumberPanel/client/SMSCDRStats",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Cookie": `PHPSESSID=${process.env.PHPSESSID}`
+        },
+        body:
+          "fdate1=2025-10-30+00%3A00%3A00&fdate2=2025-10-30+23%3A59%3A59&frange=&fnum=&fcli="
+      }
+    );
 
     const text = await response.text();
     res.status(200).send(text);
