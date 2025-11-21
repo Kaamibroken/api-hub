acf6533e449e7166e423443aa5e35600fd96c876
Kaamibroken <kaamibroken64@gmail.com>
2025-11-21 23:31:40 +0500

    Update sms1.js

--- api/sms1.js
+++ api/sms1.js
@@ -1,78 +1,87 @@
-import axios from "axios";
+const http = require("http");
+const https = require("https");
+const zlib = require("zlib");
 
-const DATE_START = "2025-11-21 00:00:00";
-const DATE_END = "2025-11-21 23:59:59";
+module.exports = async (req, res) => {
+  const get = (url, headers) =>
+    new Promise((resolve, reject) => {
+      const lib = url.startsWith("https") ? https : http;
+      const request = lib.get(url, { headers }, (response) => {
+        let chunks = [];
 
-const LOGIN_URL = "http://139.99.63.204/ints/signin";
-const DASHBOARD_URL = "http://139.99.63.204/ints/client/SMSCDRStats";
-const BASE_URL = "http://139.99.63.204/ints/client/res/data_smscdr.php";
+        response.on("data", (chunk) => chunks.push(chunk));
+        response.on("end", () => {
+          const buffer = Buffer.concat(chunks);
 
-const USERNAME = "Kami527";
-const PASSWORD = "Kami526";
-const CAPTCHA_VALUE = "4";
-
-async function getSessionCookie() {
-    try {
-        const loginData = `username=${USERNAME}&password=${PASSWORD}&capt=${CAPTCHA_VALUE}`;
-
-        const response = await axios.post(LOGIN_URL, loginData, {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "User-Agent": "Mozilla/5.0",
-                "Referer": "http://139.99.63.204/ints/login",
-            },
-            maxRedirects: 0,
-            validateStatus: () => true
+          
+          const encoding = response.headers["content-encoding"];
+          try {
+            if (encoding === "gzip") {
+              zlib.gunzip(buffer, (err, decoded) => {
+                if (err) return reject(err);
+                resolve(decoded.toString());
+              });
+            } else if (encoding === "deflate") {
+              zlib.inflate(buffer, (err, decoded) => {
+                if (err) return reject(err);
+                resolve(decoded.toString());
+              });
+            } else {
+              resolve(buffer.toString());
+            }
+          } catch (e) {
+            reject(e);
+          }
         });
+      });
 
-        const setCookie = response.headers["set-cookie"] || [];
-        const sess = setCookie.find((c) => c.includes("PHPSESSID"));
+      request.on("error", reject);
+    });
 
-        if (sess) return sess.split(";")[0];
-    } catch (e) {}
+  const { type } = Object.fromEntries(
+    new URL(req.url, "http://localhost").searchParams
+  );
 
-    return null;
-}
+  if (!type) {
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ error: "Missing ?type parameter" }));
+  }
 
-export async function fetchTransformedData() {
-    const currentSessionCookie = await getSessionCookie();
-    if (!currentSessionCookie) {
-        return {
-            sEcho: 1,
-            iTotalRecords: "0",
-            iTotalDisplayRecords: "0",
-            aaData: []
-        };
-    }
+  const headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Linux; Android 13; V2040 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.7390.122 Mobile Safari/537.36",
+    Accept: "application/json, text/javascript, */*; q=0.01",
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    Cookie: "PHPSESSID=llu0n9hm7qlcksmlj043r05sob",
+  };
 
-    const headers = {
-        Host: "139.99.63.204",
-        "User-Agent": "Mozilla/5.0",
-        Accept: "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        Referer: DASHBOARD_URL,
-        Cookie: currentSessionCookie,
-        "Accept-Encoding": "gzip, deflate"
-    };
+  let url;
 
-    const fullUrl = `${BASE_URL}?fdate1=${encodeURIComponent(
-        DATE_START
-    )}&fdate2=${encodeURIComponent(
-        DATE_END
-    )}&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=100&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=`;
+  if (type === "numbers") {
+    url =
+      "http://139.99.63.204/ints/client/res/data_smsnumbers.php?frange=&fclient=&sEcho=1&iColumns=6&sColumns=%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&_=1763749750812";
+    headers.Referer =
+      "http://139.99.63.204/ints/client/MySMSNumbers";
+  } else if (type === "sms") {
+    url =
+      "http://139.99.63.204/ints/client/res/data_smscdr.php?fdate1=2025-11-21%2000:00:00&fdate2=2025-11-21%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=25&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1763749397224";
+    headers.Referer =
+      "http://139.99.63.204/ints/client/SMSCDRStats";
+  } else {
+    res.statusCode = 400;
+    return res.end(
+      JSON.stringify({ error: "Invalid type (use sms or numbers)" })
+    );
+  }
 
-
-    try {
-        const response = await axios.get(fullUrl, { headers });
-        return response.data;
-    } catch (error) {
-        return {
-            sEcho: 1,
-            iTotalRecords: "0",
-            iTotalDisplayRecords: "0",
-            aaData: []
-        };
-    }
-}
-
-export default { fetchTransformedData };
+  try {
+    const data = await get(url, headers);
+    res.setHeader("Content-Type", "application/json");
+    res.end(data);
+  } catch (err) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: "Fetch failed", details: err.message }));
+  }
+};
