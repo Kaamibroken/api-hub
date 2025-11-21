507c8cd9f44074c82216fd011d342d37edc0f8a2
Kaamibroken <kaamibroken64@gmail.com>
2025-11-21 23:28:11 +0500

    Update sms1.js

--- api/sms1.js
+++ api/sms1.js
@@ -1,106 +1,78 @@
-const http = require("http");
-const https = require("https");
-const querystring = require("querystring");
+import axios from "axios";
 
-function GET(url, headers) {
-  return new Promise((resolve, reject) => {
-    const lib = url.startsWith("https") ? https : http;
-    const req = lib.get(url, { headers }, (res) => {
-      let data = [];
-      res.on("data", (c) => data.push(c));
-      res.on("end", () => {
-        const body = Buffer.concat(data).toString();
-        resolve({ body, headers: res.headers });
-      });
-    });
-    req.on("error", reject);
-  });
+const DATE_START = "2025-11-21 00:00:00";
+const DATE_END = "2025-11-21 23:59:59";
+
+const LOGIN_URL = "http://139.99.63.204/ints/signin";
+const DASHBOARD_URL = "http://139.99.63.204/ints/client/SMSCDRStats";
+const BASE_URL = "http://139.99.63.204/ints/client/res/data_smscdr.php";
+
+const USERNAME = "Kami527";
+const PASSWORD = "Kami526";
+const CAPTCHA_VALUE = "4";
+
+async function getSessionCookie() {
+    try {
+        const loginData = `username=${USERNAME}&password=${PASSWORD}&capt=${CAPTCHA_VALUE}`;
+
+        const response = await axios.post(LOGIN_URL, loginData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "User-Agent": "Mozilla/5.0",
+                "Referer": "http://139.99.63.204/ints/login",
+            },
+            maxRedirects: 0,
+            validateStatus: () => true
+        });
+
+        const setCookie = response.headers["set-cookie"] || [];
+        const sess = setCookie.find((c) => c.includes("PHPSESSID"));
+
+        if (sess) return sess.split(";")[0];
+    } catch (e) {}
+
+    return null;
 }
 
-function POST(url, headers, body) {
-  return new Promise((resolve, reject) => {
-    const lib = url.startsWith("https") ? https : http;
-    const req = lib.request(url, { method: "POST", headers }, (res) => {
-      let data = [];
-      res.on("data", (c) => data.push(c));
-      res.on("end", () => {
-        const body = Buffer.concat(data).toString();
-        resolve({ body, headers: res.headers });
-      });
-    });
-    req.on("error", reject);
-    req.write(body);
-    req.end();
-  });
+export async function fetchTransformedData() {
+    const currentSessionCookie = await getSessionCookie();
+    if (!currentSessionCookie) {
+        return {
+            sEcho: 1,
+            iTotalRecords: "0",
+            iTotalDisplayRecords: "0",
+            aaData: []
+        };
+    }
+
+    const headers = {
+        Host: "139.99.63.204",
+        "User-Agent": "Mozilla/5.0",
+        Accept: "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+        Referer: DASHBOARD_URL,
+        Cookie: currentSessionCookie,
+        "Accept-Encoding": "gzip, deflate"
+    };
+
+    const fullUrl = `${BASE_URL}?fdate1=${encodeURIComponent(
+        DATE_START
+    )}&fdate2=${encodeURIComponent(
+        DATE_END
+    )}&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=100&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=`;
+
+
+    try {
+        const response = await axios.get(fullUrl, { headers });
+        return response.data;
+    } catch (error) {
+        return {
+            sEcho: 1,
+            iTotalRecords: "0",
+            iTotalDisplayRecords: "0",
+            aaData: []
+        };
+    }
 }
 
-async function getSession() {
-  const postData = querystring.stringify({
-    username: "Kami527",
-    password: "Kami526",
-    capt: "4",
-  });
-
-  const headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Content-Length": Buffer.byteLength(postData),
-    "User-Agent": "Mozilla/5.0",
-    Referer: "http://139.99.63.204/ints/login",
-  };
-
-  const resp = await POST("http://139.99.63.204/ints/signin", headers, postData);
-  const set = resp.headers["set-cookie"] || [];
-  const cookie = set.find((c) => c.includes("PHPSESSID"));
-  return cookie ? cookie.split(";")[0] : null;
-}
-
-module.exports = async (req, res) => {
-  const query = Object.fromEntries(
-    new URL(req.url, "http://localhost").searchParams
-  );
-
-  const type = query.type;
-  if (!type) {
-    res.statusCode = 400;
-    return res.end(JSON.stringify({ error: "Missing ?type" }));
-  }
-
-  const session = await getSession();
-  if (!session) {
-    res.statusCode = 500;
-    return res.end(JSON.stringify({ error: "Session error" }));
-  }
-
-  let url;
-  let referer;
-
-  if (type === "sms") {
-    url =
-      "http://139.99.63.204/ints/client/res/data_smscdr.php?fdate1=2025-11-21%2000:00:00&fdate2=2025-11-21%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=200&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1";
-    referer = "http://139.99.63.204/ints/client/SMSCDRStats";
-  } else if (type === "numbers") {
-    url =
-      "http://139.99.63.204/ints/client/res/data_smsnumbers.php?frange=&fclient=&sEcho=1&iColumns=6&sColumns=%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=200";
-    referer = "http://139.99.63.204/ints/client/MySMSNumbers";
-  } else {
-    res.statusCode = 400;
-    return res.end(JSON.stringify({ error: "Invalid type" }));
-  }
-
-  const headers = {
-    "User-Agent": "Mozilla/5.0",
-    Accept: "application/json",
-    "X-Requested-With": "XMLHttpRequest",
-    Referer: referer,
-    Cookie: session,
-  };
-
-  try {
-    const result = await GET(url, headers);
-    res.setHeader("Content-Type", "application/json");
-    res.end(result.body);
-  } catch (e) {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ error: "Fetch failed" }));
-  }
-};
+export default { fetchTransformedData };
