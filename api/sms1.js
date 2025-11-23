77c25dce8216334128aa34a9e882f96cb3fde90d
Kaamibroken <kaamibroken64@gmail.com>
2025-11-23 10:10:26 +0500

    Update sms1.js

--- api/sms1.js
+++ api/sms1.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=llu0n9hm7qlcksmlj043r05sob",
+    Cookie: "PHPSESSID=odo4p6qk61oll5k2jm1pa1daa7",
   };
 
   let url;
