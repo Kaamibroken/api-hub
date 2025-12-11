3a62085fddb82b721cd41e750c483bc4d3fa3576
Kaamibroken <kaamibroken64@gmail.com>
2025-12-11 19:28:04 +0500

    Update sms.js

--- api/sms.js
+++ api/sms.js
@@ -1,112 +1,90 @@
-const axios = require('axios');
-
-const CREDENTIALS = {
-    username: "Kami522",
-    password: "Kami526"
-};
-
-const BASE_URL = "http://51.89.99.105/NumberPanel";
-const OTP_URL = "http://51.89.99.105/NumberPanel/client/res/data_smscdr.php?fdate1=2025-12-11%2000:00:00&fdate2=2425-12-11%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sesskey=Q05RRkJQUEJCUg&sEcho=2&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1765460983523";
-
-const HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest",
-    "Referer": `${BASE_URL}/client/SMSCDRStats`,
-    "Origin": "http://51.89.99.105"
-};
-
-let cachedCookie = "PHPSESSID=jd6baa99k47e8bkv1o17c9c91a";
-let failedOnce = false;
-
-async function performLogin() {
-    try {
-        const session = axios.create({ 
-            withCredentials: true, 
-            headers: HEADERS,
-            timeout: 8000
-        });
-
-        const loginPage = await session.get(`${BASE_URL}/login`);
-        
-        let tempCookie = "";
-        if (loginPage.headers['set-cookie']) {
-            const c = loginPage.headers['set-cookie'].find(x => x.includes('PHPSESSID'));
-            if (c) tempCookie = c.split(';')[0];
-        }
-
-        const match = loginPage.data.match(/What is (\d+) \+ (\d+) = \?/);
-        if (!match) throw new Error("Captcha Not Found");
-        
-        const answer = parseInt(match[1]) + parseInt(match[2]);
-
-        const params = new URLSearchParams();
-        params.append('username', CREDENTIALS.username);
-        params.append('password', CREDENTIALS.password);
-        params.append('capt', answer);
-
-        const loginResp = await session.post(`${BASE_URL}/signin`, params, {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Cookie": tempCookie
-            },
-            maxRedirects: 0,
-            validateStatus: () => true
-        });
-
-        if (loginResp.headers['set-cookie']) {
-            const newC = loginResp.headers['set-cookie'].find(x => x.includes('PHPSESSID'));
-            if (newC) {
-                cachedCookie = newC.split(';')[0];
-                return cachedCookie;
-            }
-        }
-        
-        if (tempCookie) {
-            cachedCookie = tempCookie;
-            return cachedCookie;
-        }
-
-        throw new Error("Login failed");
-
-    } catch (e) {
-        throw e;
-    }
-}
+const http = require("http");
+const https = require("https");
+const zlib = require("zlib");
 
 module.exports = async (req, res) => {
-    try {
-        let response = await axios.get(OTP_URL, {
-            headers: { ...HEADERS, "Cookie": cachedCookie },
-            validateStatus: () => true,
-            timeout: 5000
-        });
+  const get = (url, headers) =>
+    new Promise((resolve, reject) => {
+      const lib = url.startsWith("https") ? https : http;
+      const request = lib.get(url, { headers }, (response) => {
+        let chunks = [];
 
-        const isHtml = typeof response.data === 'string' && 
-                      (response.data.includes('<html') || 
-                       response.data.includes('login') || 
-                       response.data.includes('Direct Script'));
+        response.on("data", (chunk) => chunks.push(chunk));
+        response.on("end", () => {
+          const buffer = Buffer.concat(chunks);
 
-        if (isHtml) {
-            if (failedOnce) {
-                await performLogin();
-                failedOnce = false;
-
-                response = await axios.get(OTP_URL, {
-                    headers: { ...HEADERS, "Cookie": cachedCookie },
-                    validateStatus: () => true,
-                    timeout: 5000
-                });
+          
+          const encoding = response.headers["content-encoding"];
+          try {
+            if (encoding === "gzip") {
+              zlib.gunzip(buffer, (err, decoded) => {
+                if (err) return reject(err);
+                resolve(decoded.toString());
+              });
+            } else if (encoding === "deflate") {
+              zlib.inflate(buffer, (err, decoded) => {
+                if (err) return reject(err);
+                resolve(decoded.toString());
+              });
             } else {
-                failedOnce = true;
-                return res.status(response.status).send(response.data);
+              resolve(buffer.toString());
             }
-        } else {
-            failedOnce = false;
-        }
+          } catch (e) {
+            reject(e);
+          }
+        });
+      });
 
-        if (typeof response.data === 'object') {
-            return res.status(200).json(response.data);
-        } else {
+      request.on("error", reject);
+    });
+
+  const { type } = Object.fromEntries(
+    new URL(req.url, "http://localhost").searchParams
+  );
+
+  if (!type) {
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ error: "Missing ?type parameter" }));
+  }
+
+  const headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36",
+    Accept: "application/json, text/javascript, */*; q=0.01",
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    Cookie: "PHPSESSID=2f8rssdk8k5bfh6kpckss2h0g2",
+  };
+
+  let url;
+
+  if (type === "numbers") {
+    url =
+      "http://51.89.99.105/NumberPanel/client/res/data_smsnumbers.php?frange=&fclient=&sEcho=2&iColumns=6&sColumns=%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&_=1765444139131";
+    headers.Referer =
+      "http://51.89.99.105/NumberPanel/client/MySMSNumbers";
+  } else if (type === "sms") {
+    url =
+      "http://51.89.99.105/NumberPanel/client/res/data_smscdr.php?fdate1=2025-12-11%2000:00:00&fdate2=2025-12-11%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sesskey=Q05RRkJQUEJCUg&sEcho==2&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1765458023102";
+    headers.Referer =
+      "http://51.89.99.105/NumberPanel/client/SMSCDRStats";
+  } else {
+    res.statusCode = 400;
+    return res.end(
+      JSON.stringify({ error: "Invalid type (use sms or numbers)" })
+    );
+  }
+
+  try {
+    const data = await get(url, headers);
+    res.setHeader("Content-Type", "application/json");
+    res.end(data);
+  } catch (err) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: "Fetch failed", details: err.message }));
+  }
+};        } else {
             return res.status(response.status).send(response.data);
         }
 
