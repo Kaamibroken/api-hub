222dc3ba95d0b21dccba4747776fc1969018cb3f
Kaamibroken <kaamibroken64@gmail.com>
2025-12-11 19:59:51 +0500

    Update data.js

--- api/data.js
+++ api/data.js
@@ -9,13 +9,13 @@ const BASE_URL = "http://51.89.99.105/NumberPanel";
 const OTP_URL = "http://51.89.99.105/NumberPanel/client/res/data_smscdr.php?fdate1=2025-12-11%2000:00:00&fdate2=2025-12-11%2023:59:59&frange=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgnumber=&fgcli=&fg=0&sesskey=Q05RRkJQUEJCUg&sEcho=2&iColumns=7&sColumns=%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1765460983523";
 
 const HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36",
     "X-Requested-With": "XMLHttpRequest",
     "Referer": `${BASE_URL}/client/SMSCDRStats`,
     "Origin": "http://51.89.99.105"
 };
 
-let cachedCookie = "PHPSESSID=jd6baa99k47e8bkv1o17c9c91a";
+let cachedCookie = "PHPSESSID=2f8rssdk8k5bfh6kpckss2h0g2";
 let failedOnce = false;
 
 async function performLogin() {
