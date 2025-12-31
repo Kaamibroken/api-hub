7bc83242d08afd684f2923df44572edb4663ff08
Kaamibroken <kaamibroken64@gmail.com>
2025-12-31 16:13:01 +0500

    Update dua.js

--- api/dua.js
+++ api/dua.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PPHPSESSID=0d300ctlrfntj4ue8r8ouhsc3k,
+    Cookie: "PPHPSESSID=0d300ctlrfntj4ue8r8ouhsc3k",
   };
 
   let url;
